d59ad3518923b95b28bd40b68dbff6b10b11ce82
Erik Storrs <epstorrs@gmail.com>
2023-07-07 10:34:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'omero-wrapper=omero_wrapper.omero:main',
+            'omero-wrapper=omero_wrapper.main:main',
         ],
     },
 )
